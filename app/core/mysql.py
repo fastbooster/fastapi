47dccb2151fca55ebcec235c2b17733d9c33098d
Super Junior <easelify@gmail.com>
2024-07-21 01:00:25 +0800

    优化MySQL连接池

--- app/core/mysql.py
+++ app/core/mysql.py
@@ -12,6 +12,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from dbutils.pooled_db import PooledDB
 from dotenv import load_dotenv
+from app.core.log import logger
 
 load_dotenv()
 
@@ -36,14 +37,17 @@ engine = create_engine(
     'mysql+pymysql://{user}:{password}@{host}:{port}/{database}?charset={charset}'.format(
         **MYSQL_CONFIG),
     pool_pre_ping=True,  # 在每次获取连接时检查连接是否有效
-    pool_recycle=30,  # 定时自动回收连接，防止有未关闭的连接将连接池耗尽
+    pool_recycle=-1,  # 定时自动回收连接，防止有未关闭的连接将连接池耗尽
+    pool_timeout=10,  # 连接池超时时间, 此时间内从池子获取不到连接则抛出异常
     pool_size=pool_size,  # 连接池大小
+    max_overflow=pool_size * 2,
     # max_overflow=0,  # 不允许 SQLAlchemy 创建额外连接
     # poolclass=db_pool.Pool, # 使用自定义的其他连接池类
 )
 
 # 创建 ORM Session 工厂
-Session = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+SessionFactory = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+Session = scoped_session(SessionFactory)
 
 
 def get_url() -> str:
@@ -68,10 +72,14 @@ def get_session():
     """
     上下文管理器，用于获取数据库会话对象，并在上下文结束时自动关闭会话。
     """
-    db = None
+    session = None
     try:
-        db = Session()
-        yield db  # 将会话对象提供给上下文使用
+        session = Session()
+        yield session
+    except Exception as e:
+        logger.error(f"数据库操作发生异常：{e}")
+        raise
     finally:
-        if db is not None:
-            db.close()
+        if session is not None and session.is_active:
+            # session.close()
+            Session.remove()
