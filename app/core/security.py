9615e35b6cdf225339deee1dff596dd39df7c305
Super Junior <easelify@gmail.com>
2024-08-19 23:52:08 +0800

    优化登录

--- app/core/security.py
+++ app/core/security.py
@@ -5,24 +5,25 @@
 # Email: easelify@gmail.com
 # Time: 2024/05/15 21:12
 
-import os
-import json
 import hashlib
 import hmac
+import json
+import os
 import re
-from datetime import datetime, timedelta
+from datetime import datetime, timezone, timedelta
 from typing import Any
-from app.core.log import logger
 
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 
-from app.models.user import UserModel
-from app.core.redis import get_redis
-from app.core.mysql import get_session
 from app.constants.constants import REDIS_AUTH_USER_PREFIX, REDIS_AUTH_TTL
+from app.core.log import logger
+from app.core.mysql import get_session
+from app.core.redis import get_redis
+from app.models.user import UserModel
+from app.schemas.schemas import StatusType
 
 
 class BearAuthException(Exception):
@@ -71,7 +72,7 @@ def get_password_hash(password) -> str:
 
 
 def create_access_token(subject: str | Any) -> str:
-    expire = datetime.utcnow() + timedelta(seconds=REDIS_AUTH_TTL)
+    expire = datetime.now(timezone.utc) + timedelta(seconds=REDIS_AUTH_TTL)
     # 注意: sub 必须强制为字符串，否则会解码失败
     to_encode = {"exp": expire, "sub": str(subject)}
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
@@ -99,6 +100,9 @@ def authenticate_user_by_password(password: str, phone: str = None, email: str =
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN, detail="账号或密码错误")
 
+        if user.status == StatusType.DISABLED.value:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="账号已被禁用")
+
         user_data = user.__dict__
         user_data.pop('_sa_instance_state', None)
 
@@ -109,46 +113,43 @@ def get_current_user_id(token: str = Depends(oauth2_scheme)) -> int:
     """根据 token 获取用户ID"""
     try:
         user_id = get_token_payload(token)
+        return int(user_id)
     except BearAuthException as e:
         logger.warning(e)
         raise_forbidden()
-
-    return int(user_id)
 
 
 def get_current_user(token: str = Depends(oauth2_scheme)) -> dict:
     """根据 token 获取用户信息"""
     try:
         user_id = get_token_payload(token)
+        with get_session(read_only=True) as db:
+            user = db.query(UserModel).filter_by(id=user_id).first()
+            if not user:
+                raise_forbidden()
+            user_data = user.__dict__
+            user_data.pop('_sa_instance_state', None)
+            return user_data
     except BearAuthException as e:
         logger.warning(e)
         raise_forbidden()
-
-    with get_session(read_only=True) as db:
-        user = db.query(UserModel).filter_by(id=user_id).first()
-        if not user:
-            raise_forbidden()
-        user_data = user.__dict__
-        user_data.pop('_sa_instance_state', None)
-        return user_data
 
 
 def get_current_user_from_cache(token: str = Depends(oauth2_scheme)) -> dict:
     """根据 token 从登录缓存中获取用户信息"""
     try:
         user_id = get_token_payload(token)
+        with get_redis() as redis:
+            user_data = json.loads(redis.get(f'{REDIS_AUTH_USER_PREFIX}{user_id}'))
+
+        if not user_data:
+            logger.warning(f'Could not validate bearer token: {token}')
+            raise_forbidden()
+
+        return user_data
     except BearAuthException as e:
         logger.warning(e)
         raise_forbidden()
-
-    with get_redis() as redis:
-        user_data = json.loads(redis.get(f'{REDIS_AUTH_USER_PREFIX}{user_id}'))
-
-    if not user_data:
-        logger.warning(f'Could not validate bearer token: {token}')
-        raise_forbidden()
-
-    return user_data
 
 
 class AuthChecker:
@@ -169,18 +170,18 @@ class AuthChecker:
 
 
 def check_permission(component_name: str):
-    '''
-    #### 菜单权限检测
+    """#### 菜单权限检测
 
     该函数接受 component_name 参数，并返回一个内部函数 permission_checker函数
     permission_checker 函数负责实际的权限检查逻辑，并接受通过 Depends 注入的 user_id，
     这样，component_name 就可以作为一个参数传递给 check_permission，而不是直接传递给 Depends
-    TOOD: 细化权限粒度，而不是只检查菜单权限
-    '''
+    TODO: 细化权限粒度，而不是只检查菜单权限
+    """
+
     async def permission_checker(user_data: dict = Depends(get_current_user_from_cache)) -> None:
         if component_name not in user_data['permissions']:
-            logger.warning(f'用户 (id={user_data["id"]}, nickname={
-                           user_data["nickname"]}) 尝试访问未授权的权限组件 {component_name}')
+            logger.warning(
+                f'用户 (id={user_data["id"]}, nickname={user_data["nickname"]}) 尝试访问未授权的权限组件 {component_name}')
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN, detail="Forbidden")
 
