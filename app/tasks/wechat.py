d1efcba5de4dd6f7a5c9a6acb01904373f031ee6
Super Junior <easelify@gmail.com>
2025-03-30 15:26:34 +0800

    Fix: f-string error

--- app/tasks/wechat.py
+++ app/tasks/wechat.py
@@ -19,17 +19,18 @@ def refresh_access_token():
     with get_redis() as redis:
         cache_data = redis.hgetall(REDIS_WECHAT)
         if cache_data is None:
-            logger.info('系统没有添加任何微信媒体平台，忽略刷新access_token')
+            logger.info('系统没有添加任何微信媒体平台, 忽略刷新access_token')
             return
 
         for item in cache_data.items():
             config = json.loads(item[1])
+            appid = config['appid']
             if config['status'] != 'enabled':
-                logger.info(f'微信媒体平台({config['appid']})已禁用，忽略刷新access_token')
+                logger.info(f'微信媒体平台({appid})已禁用, 忽略刷新access_token')
                 return
             try:
-                client = wechat_manager.get_instance(config['appid'])
+                client = wechat_manager.get_instance(appid)
                 client.fetch_access_token()
-                logger.info(f'成功刷新微信媒体平台({config['appid']})的access_token')
+                logger.info(f'成功刷新微信媒体平台({appid})的access_token')
             except Exception as e:
-                logger.error(f'刷新微信媒体平台({config['appid']})的access_token失败：{e}')
+                logger.error(f'刷新微信媒体平台({appid})的access_token失败: {e}')
